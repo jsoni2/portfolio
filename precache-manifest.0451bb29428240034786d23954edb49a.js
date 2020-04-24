8ac7a9a46d20cb60e38b45b363002771b69a62a3
Janak Soni <janak.soni@thryv.com>
2020-04-24 14:40:41 -0500

    Updates

---
+++ precache-manifest.0451bb29428240034786d23954edb49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5481cdcf59943efec5ace970d6dffbf8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29928e329278eff95d33",
+    "url": "/portfolio/static/css/main.c7754279.chunk.css"
+  },
+  {
+    "revision": "84db052380e6a59ddc71",
+    "url": "/portfolio/static/js/2.5ba50a4f.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.5ba50a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29928e329278eff95d33",
+    "url": "/portfolio/static/js/main.cd4b62a7.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
